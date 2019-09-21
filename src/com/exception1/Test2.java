039876c84820c6295b2ce156932ff59078d012fd
PritamX Kumar <pritamx.kumar@intel.com>
2019-09-21 15:42:04 +0530

    new feature has been added

--- src/com/exception1/Test2.java
+++ src/com/exception1/Test2.java
@@ -2,9 +2,11 @@ package com.exception1;
 
 public class Test2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 
+		System.out.println("I have created new Feature Component: ");
+		
 	}
 
 }
